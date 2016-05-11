0acd5136c58a81dc4982e3ac26f6e626f5c7ce75
Brian Gormanly <brian.gormanly@marist.edu>
2016-05-11 16:51:25 -0400

    word counting under way

--- search-googlesearch.py
+++ search-googlesearch.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 from google import search
 from google import get_page
+from collections import Counter
+import re
 import sys
 import MySQLdb
 import extraction
@@ -23,7 +25,7 @@ if(len(sys.argv) > 1):
     #print ("hi")
     html = requests.get(url).text
     extracted = extraction.Extractor().extract(html, source_url=url)
-    sqlString = "insert into searchResults (url, search, title, description, imageUrl, content, feeds, rating) values (%s, %s, %s, %s, %s, %s, %s, %s);"
+    sqlString = "insert into searchResults (url, search, title, description, imageUrl, content, feeds, rating, searchWordCount, friendwordCount) values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s);"
     #print extracted.title, extracted.description, extracted.image, extracted.url
     
     soup = BeautifulSoup.BeautifulSoup(html)
@@ -31,8 +33,20 @@ if(len(sys.argv) > 1):
     for node in soup.findAll('p'):
       pageContent+=''.join(node.findAll(text=True))
     
+    #count the occurances of the search word in the site and weighted total (url = n*5, title = v*3, description v*2, content = v*1)
+    titleSearchWordCount=url.lower().count(sys.argv[1].lower()) *5
+    titleSearchWordCount+=extracted.title.lower().count(sys.argv[1].lower()) *3
+    titleSearchWordCount+=extracted.description.lower().count(sys.argv[1].lower()) *2
+    titleSearchWordCount+=pageContent.lower().count(sys.argv[1].lower())
+    
+    #count all the words in the text to find friendly words
+    descriptionWords = Counter(re.findall(r'\w+', extracted.description))
+    contentWords = Counter(re.findall(r'\w+', pageContent))
+    print(descriptionWords)
+    print(contentWords)
+    
     #print pageContent
-    cur.execute(sqlString, (url, sys.argv[1], extracted.title, extracted.description, extracted.image, pageContent, str(extracted.feeds), 1.0))
+    cur.execute(sqlString, (url, sys.argv[1], extracted.title, extracted.description, extracted.image, pageContent, str(extracted.feeds), 1.0, titleSearchWordCount, 1))
     db.commit()
     #print(extracted)
 
