64ce59a18c74524fbdf82c0a839badcd64077e07
Brian Gormanly <brian.gormanly@marist.edu>
2016-05-11 16:10:54 -0400

    page text is now being saved in the content field

--- search-googlesearch.py
+++ search-googlesearch.py
@@ -5,6 +5,7 @@ import sys
 import MySQLdb
 import extraction
 import requests
+import BeautifulSoup
 
 print(len(sys.argv))
 
@@ -19,12 +20,20 @@ if(len(sys.argv) > 1):
   print(sys.argv[1])
   for url in search(sys.argv[1], stop=8):
     print(url)
-    print ("hi")
+    #print ("hi")
     html = requests.get(url).text
     extracted = extraction.Extractor().extract(html, source_url=url)
-    sqlString = "insert into searchResults (url, search, title, description, imageUrl, content, head, rating) values (%s, %s, %s, %s, %s, %s, %s, %s);"
-    print extracted.title, extracted.description, extracted.image, extracted.url
-    cur.execute(sqlString, (url, sys.argv[1], extracted.title, extracted.description, extracted.url, " ", extracted.image, 1.0))
+    sqlString = "insert into searchResults (url, search, title, description, imageUrl, content, feeds, rating) values (%s, %s, %s, %s, %s, %s, %s, %s);"
+    #print extracted.title, extracted.description, extracted.image, extracted.url
+    
+    soup = BeautifulSoup.BeautifulSoup(html)
+    pageContent = "";
+    for node in soup.findAll('p'):
+      pageContent+=''.join(node.findAll(text=True))
+    
+    #print pageContent
+    cur.execute(sqlString, (url, sys.argv[1], extracted.title, extracted.description, extracted.image, pageContent, str(extracted.feeds), 1.0))
     db.commit()
+    #print(extracted)
 
   cur.close()
