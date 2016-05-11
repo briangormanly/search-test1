b52183682f0c498f4ff88e8090fa3ba9ec41ea85
Brian Gormanly <brian.gormanly@marist.edu>
2016-05-11 15:28:18 -0400

    big improvements to the search

--- search-googlesearch.py
+++ search-googlesearch.py
@@ -1,28 +1,30 @@
 #!/usr/bin/python
-
 from google import search
 from google import get_page
 import sys
 import MySQLdb
+import extraction
+import requests
 
 print(len(sys.argv))
 
-if( len(sys.argv) > 1 ):
-	db = MySQLdb.connect(host="localhost",    # your host, usually localhost
-                     user="root",         # your username
-                     passwd="",  # your password
-                     db="google_test1")        # name of the data base
-	# you must create a Cursor object. It will let
-	#  you execute all the queries you need
-	cur = db.cursor()
-	print(sys.argv[1])
-	for url in search(sys.argv[1], stop=8):
-		print(url)
-		#print( get_page(url))
-		#sqlString = "insert into searchResults (url, content) values ('" + url + "', '" + get_page(url) + "')"
-		sqlString = "insert into searchResults (url, content) values (%s, %s);"
-		#print(sqlString)
-		cur.execute(sqlString, (url, get_page(url)))
-		db.commit()
+if(len(sys.argv) > 1):
+  db = MySQLdb.connect(host="localhost",    # your host, usually localhost
+      user="root",         # your username
+      passwd="",  # your password
+      db="google_test1")        # name of the data base
+  # you must create a Cursor object. It will let
+  #  you execute all the queries you need
+  cur = db.cursor()
+  print(sys.argv[1])
+  for url in search(sys.argv[1], stop=8):
+    print(url)
+    print ("hi")
+    html = requests.get(url).text
+    extracted = extraction.Extractor().extract(html, source_url=url)
+    sqlString = "insert into searchResults (url, search, title, description, imageUrl, content, head, rating) values (%s, %s, %s, %s, %s, %s, %s, %s);"
+    print extracted.title, extracted.description, extracted.image, extracted.url
+    cur.execute(sqlString, (url, sys.argv[1], extracted.title, extracted.description, extracted.url, " ", extracted.image, 1.0))
+    db.commit()
 
-	cur.close()
+  cur.close()
