3d64132bc78123d451de7d173b0e385dd209756b
Brian Gormanly <brian.gormanly@marist.edu>
2016-05-13 11:28:31 -0400

    woah.. it is doing things

--- search-googlesearch.py
+++ search-googlesearch.py
@@ -18,27 +18,58 @@ db = MySQLdb.connect(host="localhost",    # your host, usually localhost
 #  you execute all the queries you need
 cur = db.cursor()
 
-def checkWord(word):
-  # see if the word is in the word table
-  #print(word)
-  wordSelect = "select * from word where word= %s;"
-  res = cur.execute(wordSelect, ([word]))
-  data = cur.fetchall()
-  if data:
-    #return the wordId
-    #print("hi")
-    #print data[0][0]
-    return data[0][0]
-  else:
+def saveResultPage(searchId, url, title, description, imageUrl, content, feeds):
+  sqlString = "insert into resultPage (searchId, url, title, description, imageUrl, content, feeds) values (%s, %s, %s, %s, %s, %s, %s);"
+  cur.execute(sqlString, (searchId, url, title, description, imageUrl, content, str(feeds)))
+  #cur.execute(wordInsert, ([word]))
+  db.commit()
+  return cur.lastrowid;
+
+def saveResultWord(resultPageId, wordId, wordCount, wordLocationId, isSearchWord):
+  sqlString = "insert into resultWord (resultPageId, wordId, wordCount, wordLocationId, isSearchWord) values (%s, %s, %s, %s, %s);"
+  cur.execute(sqlString, (resultPageId, wordId, wordCount, wordLocationId, isSearchWord))
+  #pcur.execute(wordInsert, ([word]))
+  db.commit()
+  return cur.lastrowid;
+
+def saveWord(word):
+  existingWord = getWord(word)
+  #print existingWord
+  if existingWord is -1:
     #create the word
-    #print("1 bye");
+    print("1 bye");
     wordInsert = "insert into word (word) values (%s);"
     cur.execute(wordInsert, ([word]))
     db.commit()
     #print("2 bye")
     #print db.insert_id()
     return cur.lastrowid;
+  else:
+    return existingWord
 
+def saveSearch(searchPhrase, parentId):
+  sqlString = "insert into search (searchPhrase, parentSearchId) values (%s, %s);"
+  cur.execute(sqlString, (str(searchPhrase), parentId))
+  db.commit()
+  return cur.lastrowid;
+
+def getWord(word):
+  sqlString = "select * from word where word= %s;"
+  res = cur.execute(sqlString, ([word]))
+  data = cur.fetchall()
+  if data:
+    return data[0][0]
+  else:
+    return -1
+
+def getWordLocation(wordLocation):
+  sqlString = "select * from wordLocation where location= %s;"
+  res = cur.execute(sqlString, ([wordLocation]))
+  data = cur.fetchall()
+  if data:
+    return data[0][0]
+  else:
+    return -1
 
 # words we don't care about
 discard = '[a, all, also, another, and, any, anybody, anyone, anything, are, be, both, by, each, either, everybody, everyone, everything, few, for, get, has, he, her, hers, herself, him, himself, his, i, it, its, itself, little, many, me, mine, more, most, much, my, myself, nbsp, neither, nobody, none, nothing, of, one, other, others, our, ours, ourselves, several, she, some, somebody, someone, something, that,their, theirs, them, themselves, these, the, they, this, those, to, us, was, we, what, whatever, which, whichever, while, will, who, whoever, whom, whomever, whose, with, you, your, yours, yourself, yourselves]'
@@ -56,62 +87,25 @@ if(len(sys.argv) > 1):
   searchString = ' '.join(map(str, searchPhrase))
   
   # Save the search string
-  searchSqlString = "insert into search (searchPhrase, parentSearchId) values (%s, %s);"
-  cur.execute(searchSqlString, (str(searchPhrase), 0))
-  db.commit()
+  currentSearchId = saveSearch(searchPhrase, 0)
   
   #print(searchString)
-  for url in search(searchString, stop=8):
-    #print(url)
-    #print ("hi")
+  for url in search(searchString, stop=4):
+    
     html = requests.get(url).text
     extracted = extraction.Extractor().extract(html, source_url=url)
-    #print extracted.title, extracted.description, extracted.image, extracted.url
     
     soup = BeautifulSoup.BeautifulSoup(html)
     pageContent = "";
     for node in soup.findAll('p'):
       pageContent+=''.join(node.findAll(text=True))
-      
-    
-    #count the occurances of the search word in the site and weighted total (url = n*5, title = v*3, description v*2, content = v*1)
-    urlSearchWordCount=0
-    for word in searchPhrase:
-      if word in url.lower():
-        #check to see the word exists and get id
-        wordId = checkWord(word)
-        print(wordId)
-        urlSearchWordCount+=5
-    
-    titleSearchWordCount=0
-    for word in searchPhrase:
-      if word in extracted.title.lower():
-        titleSearchWordCount+=3
-        
-    descriptionSearchWordCount=0
-    for word in searchPhrase:
-      if word in extracted.description.lower():
-        descriptionSearchWordCount+=2
-        
-    ContentSearchWordCount=0
-    for word in searchPhrase:
-      if word in pageContent.lower():
-        ContentSearchWordCount+=1
-    
-    #print(urlSearchWordCount)
-    #print(titleSearchWordCount)
-    #print(descriptionSearchWordCount)
-    #print(ContentSearchWordCount)
-    
-    #titleSearchWordCount+=extracted.title.lower().count(searchPhrase.lower()) *3
-    #titleSearchWordCount+=extracted.description.lower().count(searchPhrase.lower()) *2
-    #titleSearchWordCount+=pageContent.lower().count(searchPhrase.lower())
     
     #count all the words in the text to find friendly words
+    rawTitle = re.sub("[^\w]", " ", extracted.title.lower()).split()
     rawDescription = re.sub("[^\w]", " ", extracted.description.lower()).split()
     rawContent = re.sub("[^\w]", " ", pageContent.lower()).split()
     
-    #rawDescriptionMinusPronouns = list(set(rawDescription) - set(pronouns))
+    rawTitleMinusDiscard = [desWord for desWord in rawTitle if desWord not in discard]
     rawDescriptionMinusDiscard = [desWord for desWord in rawDescription if desWord not in discard]
     rawContentMinusDiscard = [contentWord for contentWord in rawContent if contentWord not in discard]
     
@@ -132,6 +126,82 @@ if(len(sys.argv) > 1):
       if(cWord[1] > significantNumber):
         significantContentWords+=cWord
       #print(cWord[1])
+    
+    #save this page
+    currentResultPageId = saveResultPage(currentSearchId, url, extracted.title.lower(), extracted.description.lower(), extracted.image, pageContent, str(extracted.feeds))
+    
+    #word breakdown
+    for word in searchPhrase:
+      if word in url.lower():
+        #check to see the word exists and get id
+        currentWordId = saveWord(word)
+        #print(wordId)
+        saveResultWord(currentResultPageId, currentWordId, 6, getWordLocation("url"), True)
+    
+    for word in rawTitleMinusDiscard:
+      currentWordId = saveWord(word)
+      if word in searchPhrase:
+        saveResultWord(currentResultPageId, currentWordId, 4, getWordLocation("title"), True)
+      else:
+        saveResultWord(currentResultPageId, currentWordId, 2, getWordLocation("title"), False)
+    
+    for word in rawDescriptionMinusDiscard:
+      currentWordId = saveWord(word)
+      if word in searchPhrase:
+        saveResultWord(currentResultPageId, currentWordId, 3, getWordLocation("description"), True)
+      else:
+        saveResultWord(currentResultPageId, currentWordId, 1, getWordLocation("description"), False)
+
+    for word in rawContentMinusDiscard:
+      currentWordId = saveWord(word)
+      if word in searchPhrase:
+        saveResultWord(currentResultPageId, currentWordId, 3, getWordLocation("content"), True)
+      elif word in significantContentWords:
+        saveResultWord(currentResultPageId, currentWordId, 2, getWordLocation("content"), False)
+      else:
+        saveResultWord(currentResultPageId, currentWordId, 1, getWordLocation("content"), False)
+    
+        
+      #save the result word
+      #saveResultWord(currentResultPageId, currentWordId, wordCount, titleWordCount, descriptionWordCount, contentWordCount)
+
+    
+            
+    #count the occurances of the search word in the site and weighted total (url = n*5, title = v*3, description v*2, content = v*1)
+    #urlSearchWordCount=0
+    #for word in searchPhrase:
+    #  if word in url.lower():
+    #    #check to see the word exists and get id
+    #    wordId = saveWord(word)
+    #    #print(wordId)
+    #    urlSearchWordCount+=5
+    #    saveResultPage(currentSearchId,)
+    
+    #titleSearchWordCount=0
+    #for word in searchPhrase:
+    #  if word in extracted.title.lower():
+    #    titleSearchWordCount+=3
+        
+    #descriptionSearchWordCount=0
+    #for word in searchPhrase:
+    #  if word in extracted.description.lower():
+    #    descriptionSearchWordCount+=2
+        
+    #ContentSearchWordCount=0
+    #for word in searchPhrase:
+    #  if word in pageContent.lower():
+    #    ContentSearchWordCount+=1
+    
+    #print(urlSearchWordCount)
+    #print(titleSearchWordCount)
+    #print(descriptionSearchWordCount)
+    #print(ContentSearchWordCount)
+    
+    #titleSearchWordCount+=extracted.title.lower().count(searchPhrase.lower()) *3
+    #titleSearchWordCount+=extracted.description.lower().count(searchPhrase.lower()) *2
+    #titleSearchWordCount+=pageContent.lower().count(searchPhrase.lower())
+    
+    
     
     #print()
     #print(descriptionWords)
--- sql
+++ sql
@@ -3,7 +3,12 @@ use google_test1;
 create table search (id bigint primary key auto_increment, searchPhrase varchar(200), parentSearchId bigint, searchTimestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP);
 create table word (id bigint primary key auto_increment, word varchar(150), createTimestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP, updateTimestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP);
 create table resultPage (id bigint primary key auto_increment, searchId bigint, url varchar(300), title varchar(300), description TEXT, imageUrl varchar(300), content LONGTEXT, feeds TEXT, createTimestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP, updateTimestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP);
-create table resultWord (id bigint primary key auto_increment, resultPageId bigint, wordId bigint, urlWordCount int, titleWordCount int, descriptionWordCount int, contentWordCount int, isSearchWord boolean); 
+create table wordLocation (id bigint primary key auto_increment, location varchar(200), createTimestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP, updateTimestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP);
+insert into wordLocation (location) values ("url");
+insert into wordLocation (location) values ("title");
+insert into wordLocation (location) values ("description");
+insert into wordLocation (location) values ("content");
+create table resultWord (id bigint primary key auto_increment, resultPageId bigint, wordId bigint, wordCount int, wordLocationId int, isSearchWord boolean); 
 
 
 #create table searchResults (id bigint primary key auto_increment, search varchar(200), url varchar(300), title varchar(300), description TEXT, imageUrl varchar(300), content LONGTEXT, feeds TEXT, searchWordCount int, friendword varchar(100), friendwordCount int, scrapeTimestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP, updateTimestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, rating double);
