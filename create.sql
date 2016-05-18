f8a71a8562a10ee9b6b5afa238672d6418e7ff1f
Brian Gormanly <brian.gormanly@marist.edu>
2016-05-18 10:26:26 -0400

    fix for TEXT columns not being big enough

--- create.sql
+++ create.sql
@@ -4,7 +4,7 @@ create database google_test1 character set utf8;
 use google_test1;
 create table search (id bigint primary key auto_increment, searchPhrase varchar(200), parentSearchId bigint, searchOpinionScore double, searchTimestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP);
 create table word (id bigint primary key auto_increment, word varchar(250), createTimestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP, updateTimestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP);
-create table resultPage (id bigint primary key auto_increment, searchId bigint, url varchar(300), title varchar(300), description TEXT, imageUrl varchar(300), content LONGTEXT, feeds TEXT, pageOpinionScore double, createTimestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP, updateTimestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP);
+create table resultPage (id bigint primary key auto_increment, searchId bigint, url varchar(300), title varchar(300), description MEDIUMTEXT, imageUrl varchar(300), content LONGTEXT, feeds TEXT, pageOpinionScore double, createTimestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP, updateTimestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP);
 create table wordLocation (id bigint primary key auto_increment, location varchar(200), createTimestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP, updateTimestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP);
 insert into wordLocation (location) values ("url");
 insert into wordLocation (location) values ("title");
